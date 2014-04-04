412d2e4a5e3d7c6c1835bceec8f358a15de09fe7
Matt Todd <matt.c.todd@gmail.com>
2014-04-04 14:46:52 -0500

    fixed adduser

--- node/bootstrap.sh
+++ node/bootstrap.sh
@@ -7,9 +7,12 @@ sudo add-apt-repository ppa:chris-pea/node.js
 sudo apt-get update
 sudo apt-get install -y nodejs
 
+USER_NAME=deploy
+APP_NAME=node-couchbase-example
+
 # setup deploy account
-sudo adduser deploy
-sudo passwd -l deploy
+sudo adduser --disabled-password --gecos "Deploy User" $USER_NAME
+sudo passwd -l $USER_NAME
 
 # setup deploy directory
 deploy_to=/var/node/$APP_NAME
