93f8cb602826ef983cb225d1f6810cf21821ba10
Matt Todd <matt.c.todd@gmail.com>
2014-04-11 06:22:25 -0500

    added more to the node box

--- node-bootstrap.sh
+++ node-bootstrap.sh
@@ -1,8 +1,36 @@
 ﻿
 # install latest nodejs
-sudo apt-get update
-sudo apt-get install -y software-properties-common python-software-properties
-sudo add-apt-repository -y ppa:chris-lea/node.js
-sudo apt-get update
-sudo apt-get install -y nodejs
+apt-get update
+apt-get install -y software-properties-common python-software-properties
+add-apt-repository -y ppa:chris-lea/node.js
+apt-get update
+apt-get install -y nodejs
 
+USER_NAME=node
+APP_NAME=example-app
+
+# setup deploy account
+adduser --disabled-password --gecos "Node User" $USER_NAME
+passwd -l $USER_NAME
+
+# setup deploy directory
+deploy_to=/var/node/$APP_NAME
+mkdir -p ${deploy_to}
+
+cat > ${deploy_to}/app.js <<-'EOF'
+var http = require('http');
+
+var server = http.createServer(function (request, response) {
+   console.log('Received request');
+   response.writeHead(200, {"Content-Type": "text/plain"});
+   response.end("Hello World\n");
+});
+
+var port = 8000;
+server.listen(port, function() {
+   console.log('node listening on port: ' + port);
+});
+
+EOF
+
+chown -R deploy:deploy ${deploy_to}
