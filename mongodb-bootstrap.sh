405bf0dfd8a9dd8f349f3fbd8f113a492af51cbc
Matt Todd <matt.c.todd@gmail.com>
2014-05-03 17:54:41 -0500

    fixed connection errors w database

--- mongodb-bootstrap.sh
+++ mongodb-bootstrap.sh
@@ -1,19 +1,29 @@
+wait_for_db() {
+   echo "wating for db to startup..."
+   retval=1
+   while [ "$retval" -ne "0" ]
+   do
+      mongo --eval 'version()' &> /dev/null
+      retval=$?
+   done
+}
+
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
 
 apt-get update
 apt-get install -y --force-yes mongodb-org 
 
-sleep 5
+wait_for_db
 
-cat /vagrant/mongo-scripts/admin-user.js | mongo
+cat /vagrant/mongo-scripts/admin-user.js | mongo localhost/admin
 
 sed -i '/#auth = true/c\auth = true' /etc/mongod.conf
 
 service mongod restart
 
-sleep 5
+wait_for_db
 
-cat /vagrant/mongo-scripts/kanban-js-user.js | mongo -u administrator -p 'P@$$W0rd' admin
+cat /vagrant/mongo-scripts/kanban-js-user.js | mongo -u administrator -p 'P@$$W0rd' localhost/admin
 
 
