2ac9980dfdc068d178cc800a58104a077fcc193b
Matt Todd <matt.c.todd@gmail.com>
2014-05-03 13:57:57 -0500

    installing newer version of mongo

--- mongodb-bootstrap.sh
+++ mongodb-bootstrap.sh
@@ -1,5 +1,7 @@
-﻿
+﻿apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/mongodb.list
+
 # dependencies
-sudo apt-get update
-sudo apt-get install -y mongodb
+apt-get update
+apt-get install -y --force-yes mongodb-org 
 
