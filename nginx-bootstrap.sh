411583f7d330b1102531380b876ab690429b7f6b
Matt Todd <matt.c.todd@gmail.com>
2014-05-04 16:55:43 -0500

    moved config file out of bootstrap

--- nginx-bootstrap.sh
+++ nginx-bootstrap.sh
@@ -13,31 +13,7 @@ mkdir -p /var/log/nginx
 #/etc/nginx/sites-available/node
 #-------------------------------------------------------
 
-cat > /etc/nginx/sites-available/node <<-'EOF'
-
-# the IP(s) on which your node server is running. I chose port 3000.
-upstream node_app {
-    server 192.168.33.11:3000;
-}
-
-# the nginx server instance
-server {
-    listen 0.0.0.0:80;
-    access_log /var/log/nginx/node_app.log;
-
-    # pass the request to the node.js server with the correct headers and much more can be added, see nginx config options
-    location / {
-      proxy_set_header X-Real-IP $remote_addr;
-      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-      proxy_set_header Host $http_host;
-      proxy_set_header X-NginX-Proxy true;
-      proxy_set_header x-user $remote_user;
-
-      proxy_pass http://node_app/;
-      proxy_redirect off;
-    }
- }
-EOF
+cp /vagrant/nginx-config/node /etc/nginx/sites-available/node
 
 cd /etc/nginx/sites-enabled
 ln -s /etc/nginx/sites-available/node node
